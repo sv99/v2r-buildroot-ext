762b291c36fee81ba9bac750d1984af7e1c8c329
sv99 <sv99@inbox.ru>
2015-12-16 14:49:49 +0300

    add hash python-sqlalchemy

--- package/python-sqlalchemy/python-sqlalchemy.mk
+++ package/python-sqlalchemy/python-sqlalchemy.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 PYTHON_SQLALCHEMY_VERSION = 1.0.10
-PYTHON_SQLALCHEMY_SOURCE = sqlalchemy-$(PYTHON_FUTURES_VERSION).tar.gz
-PYTHON_SQLALCHEMY_SITE = http://pypi.python.org/packages/source/s/sqlalchemy
+PYTHON_SQLALCHEMY_SOURCE = SQLAlchemy-$(PYTHON_SQLALCHEMY_VERSION).tar.gz
+PYTHON_SQLALCHEMY_SITE = http://pypi.python.org/packages/source/S/SQLAlchemy
 PYTHON_SQLALCHEMY_SETUP_TYPE = setuptools
 PYTHON_SQLALCHEMY_LICENSE = MIT
 PYTHON_SQLALCHEMY_LICENSE_FILES = LICENSE.txt
