fc0e59e3399cab2b5304dee6a6c52a30ee63c7cf
sv99 <sv99@inbox.ru>
2015-12-16 14:07:51 +0300

    add hash python-backports-abc

--- package/python-backports-abc/python-backports-abc.mk
+++ package/python-backports-abc/python-backports-abc.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_BACPORTS_ABC_VERSION = 0.4
-PYTHON_BACPORTS_ABC_SOURCE = backports_abc-$(PYTHON_BACPORTS_ABC_VERSION).tar.gz
-PYTHON_BACPORTS_ABC_SITE = http://pypi.python.org/packages/source/b/backports_abc
-PYTHON_BACPORTS_ABC_SETUP_TYPE = setuptools
-PYTHON_BACPORTS_ABC_LICENSE = BSD
-PYTHON_BACPORTS_ABC_LICENSE_FILES = LICENSE.txt
+PYTHON_BACKPORTS_ABC_VERSION = 0.4
+PYTHON_BACKPORTS_ABC_SOURCE = backports_abc-$(PYTHON_BACKPORTS_ABC_VERSION).tar.gz
+PYTHON_BACKPORTS_ABC_SITE = http://pypi.python.org/packages/source/b/backports_abc
+PYTHON_BACKPORTS_ABC_SETUP_TYPE = setuptools
+PYTHON_BACKPORTS_ABC_LICENSE = BSD
+PYTHON_BACKPORTS_ABC_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
