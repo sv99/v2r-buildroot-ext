ea7582fe09af8a162c40d96b6c3bb87110bfe420
sv99 <sv99@inbox.ru>
2016-06-23 17:20:32 +0300

    add edids and capture scripts

---
+++ board/virt2real/v2/rootfs-overlay/usr/bin/capture_1080.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+WIDTH=1920
+HEIGHT=1080
+DEST=~/t${WIDTH}.jpeg
+
+if [ ! -z "$1" ]
+then
+DEST=$1
+fi
+echo "Dest: $DEST"
+
+gst-launch -v -e v4l2src always-copy=FALSE chain-ipipe=true num-buffers=1  ! \
+video/x-raw-yuv,format=\(fourcc\)NV12, width=$WIDTH, height=$HEIGHT ! \
+dmaiaccel ! dmaienc_jpeg ! filesink location=$DEST
---
+++ board/virt2real/v2/rootfs-overlay/usr/bin/capture_720.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+WIDTH=1280
+HEIGHT=720
+DEST=~/t${WIDTH}.jpeg
+
+if [ ! -z "$1" ]
+then
+    DEST=$1
+fi
+echo "Dest: $DEST"
+
+gst-launch -v -e v4l2src always-copy=FALSE chain-ipipe=true num-buffers=1  ! \
+ video/x-raw-yuv,format=\(fourcc\)NV12, width=$WIDTH, height=$HEIGHT ! \
+ dmaiaccel ! dmaienc_jpeg ! filesink location=$DEST
